32523438bb32146609f3eeb3bb0e01c0a583b9e2
Rodrigo Tavares <rodrigost23@gmail.com>
2019-07-17 09:50:15 -0400

    Fix routing not working correctly for Netlify

--- src/App.vue
+++ src/App.vue
@@ -33,14 +33,16 @@ import VueRouter from "vue-router";
 // import Home from './components/Home';
 const Home = () => import("./components/Home");
 const Create = () => import("./components/Create");
+const NotFound = () => import("./components/NotFound");
 
 const router = new VueRouter({
   mode: "history",
   base: __dirname,
   routes: [
     { path: "/", component: Home },
-    { path: "/new", component: Create }
-    ]
+    { path: "/new", component: Create },
+    { path: "*", component: NotFound }
+  ]
 });
 
 export default {
---
+++ src/components/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex xs12>
+        <v-icon size="200">mdi-file-question</v-icon>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">Page not found</h1>
+        <p class="subheading font-weight-regular">The page you are looking for doesn't exist.</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  })
+};
+</script>
+
+<style>
+</style>
