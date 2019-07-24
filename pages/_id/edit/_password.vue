2f9e3d4be7ec5ac944ec3026f3d377d8595673b6
Rodrigo Tavares <rodrigost23@gmail.com>
2019-07-24 11:53:57 -0400

    Check password before loading page

--- pages/_id/edit/_password.vue
+++ pages/_id/edit/_password.vue
@@ -12,20 +12,25 @@ import poll from '~/components/Poll.vue'
 
 export default {
   components: { poll },
-  async asyncData({ app, params, error }) {
+  async asyncData({ app, params, redirect }) {
     const pollId = params.id
     try {
-      const pollData = await app.$fireStore
-        .collection('polls')
-        .doc(pollId)
-        .get()
+      const pollRef = app.$fireStore.collection('polls').doc(pollId)
+
+      await app.$fireAuth.signInWithEmailAndPassword(
+        params.password + '@ranked.vote',
+        params.password
+      )
+
+      await pollRef.update({})
+
       return {
         pollId,
-        pollData: pollData.data(),
+        pollData: (await pollRef.get()).data(),
         pollPassword: params.password
       }
     } catch (e) {
-      error({ statusCode: 404 })
+      redirect(403, '/' + params.id)
     }
   }
 }
